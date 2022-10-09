0666872adfb62a13f3c4fd218e8646df122cd1e8
Prashant Kumar <32291207+krprashant15@users.noreply.github.com>
2022-10-09 11:41:25 +0530

    Create if_else.py

---
+++ if_else.py
@@ -0,0 +1,19 @@
+# Python If-Else with logical operators
+
+a = 10
+b = 5
+
+if a<b :
+	print("True Condition - a is less than b !!")
+else:
+	print("False Condition - a is not less than b !!")
+
+
+name = input("Enter your name (Must be a string) : ")
+age = input("Enter your age (Must be a integer) : ")
+age = int(age)
+
+if (name == "Prashant") and (age == 28):
+	print("Prashant's Data Matched !!")
+else:
+	print("Prashant's Data Mis-Matched !!")
